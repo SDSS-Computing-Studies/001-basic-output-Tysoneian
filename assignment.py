a525faea5ed5a49bd7baa8db29ef7fae85a86203
Tysoneian <99276768+Tysoneian@users.noreply.github.com>
2022-02-10 09:14:18 -0800

    code
    assingment

--- assignment.py
+++ assignment.py
@@ -1,4 +1,4 @@
-#! python3
+#!python3
 
 """
 (2 points) Modify the existing assignment file called "assignment.py".  It should display the message "Hello World!" Note that the output is case sensitive. Make sure that it is an exact match
@@ -8,3 +8,4 @@ Sample output:
 Hello World!
 
 """
+print("Hello World!")
